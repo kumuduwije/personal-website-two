4e7d8489bc8069aea22dfeaa572caa99ed5d2b7a
Kumudu Wijewardhana <kumuduwijewardana3@gmail.com>
2022-08-28 20:09:24 +0530

    Added writer effect to Hero section
    Added writer effect to Hero section and added a back to top button.

--- src/js/script.js
+++ src/js/script.js
@@ -27,3 +27,45 @@ var swiper = new Swiper(".slide-content", {
       },
   },
 });
+
+
+// Back to top JS
+
+let topBtn = document.getElementById("toTop");
+        window.onscroll = function() {screenScroll()};
+        function screenScroll(){
+            if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+                topBtn.style.display = "block";
+            }else{
+                topBtn.style.display = "none";
+            }
+        }
+
+function topClick(){
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+}
+
+// Type writter
+
+const texts = ["Undergraduate student.", "Designer.", "Programmer."]
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type(){
+  if(count === texts.length){
+    count =0;
+  }
+  currentText = texts[count];
+  letter = currentText.slice(0,++index);
+
+  document.querySelector(".typing").textContent = letter;
+  if(letter.length === currentText.length){
+    count++;
+    index = 0;
+  }
+  setTimeout(type,300);
+}());
