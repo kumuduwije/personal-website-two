dbbec23fc31e3e14759efae09d153f0eb7dd0cfe
Kumudu Wijewardhana <kumuduwijewardana3@gmail.com>
2022-08-30 12:07:01 +0530

    Started contact form validation
    Started contact form validation including css

--- src/js/script.js
+++ src/js/script.js
@@ -78,3 +78,28 @@ let letter = "";
 }
 
 typewriter();
+
+
+//Form validation 
+
+function validateForm(event) {
+  // event.preventDefault();
+  let namestatus = document.forms["contactForm"]["name"].value;
+  let mailstatus = document.forms["contactForm"]["email"].value;
+
+  if (namestatus == "") {
+    // alert("Name must be filled out");
+    document.getElementById("name-err").style.display = "block";
+    document.getElementById("name").style.border = "0.5px solid rgb(183, 66, 66)";
+    
+  }
+
+  if(mailstatus == "") {
+    document.getElementById("email-err").style.display = "block";
+    document.getElementById("email").style.border = "0.5px solid rgb(183, 66, 66)";
+  }
+
+  if(mailstatus == "" || namestatus == "") {
+    return false;
+  }
+}
